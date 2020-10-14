7614a835e4a112499f94d8063bc45ef80ad03a20
anders <andersb@tuta.io>
2020-10-14 02:23:34 -0500

    add tree-builder

---
+++ src/tree-builder.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const conflictError = () => new Error('conflict while building upload tree')
+const emptyDir = () => ({ children: {} })
+const removeSlash = (path) => path.slice(path.startsWith('/') ? 1 : 0)
+
+module.exports = async function (source) {
+  const tree = emptyDir()
+  for await (const object of source) {
+    const isFile = Boolean(object.content)
+    const pathNames = removeSlash(object.path).split('/')
+    const lastDirName = isFile ? -1 : pathNames.length
+    const lastDir = pathNames.slice(0, lastDirName)
+      .reduce((dir, name) => {
+        const exists = dir.children[name]
+        if (exists && exists.content) throw conflictError()
+        if (!exists) dir.children[name] = emptyDir()
+        return dir.children[name]
+      }, tree)
+    if (isFile) lastDir.children[pathNames[pathNames.length - 1]] = object
+  }
+
+  function traverse (dir = tree, path = '') {
+    return Object.keys(dir.children).sort()
+      .reduce((array, c) => {
+        const child = dir.children[c]
+        const childPath = `${path}/${c}`
+        return array.concat(
+          child.content
+            ? child
+            : [{ path: childPath }, ...traverse(child, childPath)]
+        )
+      }, [])
+  }
+
+  return { traverse }
+}
