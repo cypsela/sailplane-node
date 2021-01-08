5818bd1f70b7e2d021b4272f7a7d8cbbdc1e7f22
anders <andersb@tuta.io>
2021-01-08 02:57:36 -0600

    change to concatBuffers util

--- src/util/buffer.js
+++ src/util/buffer.js
@@ -5,12 +5,12 @@ const buf2hex = (ab) => ab.reduce((hex, byte) => hex + ('00' + byte.toString(16)
 const hex2buf = (hex) => Uint8Array.from(hex.match(/.{2}/g).map(h => parseInt(h, 16)))
 
 const concatBuffers = async (content, { handleUpdate, total } = {}) => {
-  let chunks = Uint8Array.from([])
+  const chunks = []
   for await (const chunk of content) {
-    chunks = concatUint8([chunks, chunk])
+    chunks.push(chunk)
     if (handleUpdate) handleUpdate(chunks.length, total)
   }
-  return chunks
+  return concatUint8(chunks)
 }
 
 module.exports = {
